defe12c84aafc7dce5811661446806d7d17f447a
mybloodyusername <y.sanaei94@gmail.com>
2026-08-07 21:10:03 +0330

    feat: mithra db context

--- Mithra.Infra/Data/MithraDbContext.cs
+++ Mithra.Infra/Data/MithraDbContext.cs
@@ -2,19 +2,20 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Mithra.Domain.Entities;
+using Mithra.Infra.Configurations;
 
 namespace Mithra.Infra.Data;
 
 public class MithraDbContext(DbContextOptions<MithraDbContext> options)
-    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
 {
     
     // TODO:public DbSet<ENTITY> Entity => Set<Entity>();
     
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        // TODO: builder.ApplyConfiguration(new ApplicationUserConfiguration());
         base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserConfig());
     }
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
